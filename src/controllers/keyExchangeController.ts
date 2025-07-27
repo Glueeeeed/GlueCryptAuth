d9c9544fcde8228a16fcd22eff5dcd12c0d3b925
glueeeeed <swiderskikuba12@gmail.com>
2025-07-27 21:55:41 +0200

    feat: Secured session secrets stored in map

--- src/controllers/keyExchangeController.ts
+++ src/controllers/keyExchangeController.ts
@@ -1,6 +1,8 @@
 import e, { Request, Response } from 'express';
 import { ec as EC} from 'elliptic';
 import KeyPair = EC.KeyPair;
+import {colors} from "../utils/chalk";
+import crypto from "crypto";
 const ec = new EC('p256');
 
 
@@ -19,6 +21,7 @@ interface KeyExchangeResponse {
 
 
 export const keyExchangeController = (req: Request<{}, {}, KeyExchangeRequest>, res: Response<KeyExchangeResponse | { error: string }>): void => {
+    console.group(colors.category('KeyExchangeController'));
     const { clientPublicKey } = req.body;
 
 
@@ -30,14 +33,17 @@ export const keyExchangeController = (req: Request<{}, {}, KeyExchangeRequest>, 
         const secret : string = serverPairKeys.derive(clientPublicKeyObj.getPublic()).toString('hex');
         const slicedSecret : string = secret.slice(0, 32);
 
-        const sessionID : string = Math.random().toString(36).substring(2, 15);
+        const sessionID : string = crypto.randomBytes(10).toString('base64');
         Secrets.set(sessionID, slicedSecret);
 
 
-         res.json({ serverPublicKey, sessionID } as KeyExchangeResponse);
+        res.json({ serverPublicKey, sessionID } as KeyExchangeResponse);
     } catch (e) {
-         res.status(400).json({ error: (e as Error).message });
+        console.error(e);
+        res.status(400).json({ error: 'Server Internal Error' });
     }
+    console.log(colors.success('Generated session key!'));
+    console.groupEnd();
 
 };
 
@@ -48,4 +54,22 @@ export const getSlicedSecret = (sessionID: string): string => {
     }
     return secret;
 };
+
+export const deleteSecret = (sessionID: string) : void => {
+
+    try {
+        if (Secrets.delete(sessionID)) {
+            console.group(colors.category('KeyExchangeController'));
+            console.log(colors.warning(`Cleared session:`, sessionID));
+            console.groupEnd();
+        } else {
+            throw new Error('Invalid session ID');
+        }
+    } catch (e) {
+        console.group(colors.category('KeyExchangeController'));
+        console.error(colors.error(e));
+        console.group(colors.category('KeyExchangeController'));
+    }
+
+}
 
